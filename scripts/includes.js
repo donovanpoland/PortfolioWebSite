7b188ad9b74b5a931c1eb4cc4021559b7edc6b57
donovanpoland <dpoland@byupathway.edu>
2025-04-12 16:26:15 -0600

    updated include.js / added about and home page paragraph

--- scripts/includes.js
+++ scripts/includes.js
@@ -12,6 +12,27 @@ async function loadHTML(id, file) {
   
   // Call includes on page load
   window.addEventListener("DOMContentLoaded", () => {
+  // Check for header placeholder
+  const headerEl = document.getElementById("header-include");
+  if (headerEl) {
     loadHTML("header-include", "header.html");
+  }
+
+  // Check for footer placeholder
+  const footerEl = document.getElementById("footer-include");
+  if (footerEl) {
     loadHTML("footer-include", "footer.html");
+  }
+
+  // Check for home page intro placeholder
+  const homeIntroEl = document.getElementById("home-intro-include");
+  if (homeIntroEl) {
+    loadHTML("home-intro-include", "home-intro.html");
+  }
+
+  // Check for about me placeholder
+  const aboutMeEl = document.getElementById("about-me-include");
+  if (aboutMeEl) {
+    loadHTML("about-me-include", "about-me.html");
+  }
   });
