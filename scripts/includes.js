3a0472d925846c589c642c5639322e8b84c34d44
donovanpoland <dpoland@byupathway.edu>
2025-04-13 21:49:24 -0600

    tweaks

--- scripts/includes.js
+++ scripts/includes.js
@@ -15,24 +15,24 @@ async function loadHTML(id, file) {
   // Check for header placeholder
   const headerEl = document.getElementById("header-include");
   if (headerEl) {
-    loadHTML("header-include", "header.html");
+    loadHTML("header-include", "includes/header.html");
   }
 
   // Check for footer placeholder
   const footerEl = document.getElementById("footer-include");
   if (footerEl) {
-    loadHTML("footer-include", "footer.html");
+    loadHTML("footer-include", "includes/footer.html");
   }
 
   // Check for home page intro placeholder
   const homeIntroEl = document.getElementById("home-intro-include");
   if (homeIntroEl) {
-    loadHTML("home-intro-include", "home-intro.html");
+    loadHTML("home-intro-include", "includes/home-intro.html");
   }
 
   // Check for about me placeholder
   const aboutMeEl = document.getElementById("about-me-include");
   if (aboutMeEl) {
-    loadHTML("about-me-include", "about-me.html");
+    loadHTML("about-me-include", "includes/about-me.html");
   }
   });
